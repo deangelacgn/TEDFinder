6eaa902b16204d441a9241e0762b6787888a46ae
rqroz <rqroz1@gmail.com>
2017-12-04 16:15:47 -0300

    counter

--- tedfinder/website/static/third-party/theme/assets/js/main.js
+++ tedfinder/website/static/third-party/theme/assets/js/main.js
@@ -5,7 +5,7 @@ $(window).load(function () { // makes sure the whole site is loaded
 })
 $(document).ready(function () {
 
-    
+
     $('input').iCheck({
         checkboxClass: 'icheckbox_square-yellow',
         radioClass: 'iradio_square-yellow',
@@ -42,11 +42,11 @@ $(document).ready(function () {
         mouseDrag: false,
         transitionStyle: "fade"
                 // "singleItem:true" is a shortcut for:
-                // items : 1, 
+                // items : 1,
                 // itemsDesktop : false,
                 // itemsDesktopSmall : false,
                 // itemsTablet: false,
-                // itemsMobile : false 
+                // itemsMobile : false
     });
     $("#prop-smlr-slide_0").owlCarousel({
         navigation: false, // Show next and prev buttons
@@ -84,41 +84,13 @@ $(document).ready(function () {
 
     setTimeout(function () {
         $('#counter').text('0');
-        $('#counter1').text('0');
-        $('#counter2').text('0');
-        $('#counter3').text('0');
         setInterval(function () {
             var curval = parseInt($('#counter').text());
-            var curval1 = parseInt($('#counter1').text().replace(' ', ''));
-            var curval2 = parseInt($('#counter2').text());
-            var curval3 = parseInt($('#counter3').text());
-            if (curval <= 1007) {
-                $('#counter').text(curval + 1);
-            }
-            if (curval1 <= 1280) {
-                $('#counter1').text(sdf_FTS((curval1 + 20), 0, ' '));
-            }
-            if (curval2 <= 145) {
-                $('#counter2').text(curval2 + 1);
-            }
-            if (curval3 <= 1022) {
-                $('#counter3').text(curval3 + 1);
+            if (curval <= 1999) {
+                $('#counter').text(curval + 2);
             }
         }, 2);
     }, 500);
-
-    function sdf_FTS(_number, _decimal, _separator) {
-        var decimal = (typeof (_decimal) != 'undefined') ? _decimal : 2;
-        var separator = (typeof (_separator) != 'undefined') ? _separator : '';
-        var r = parseFloat(_number)
-        var exp10 = Math.pow(10, decimal);
-        r = Math.round(r * exp10) / exp10;
-        rr = Number(r).toFixed(decimal).toString().split('.');
-        b = rr[0].replace(/(\d{1,3}(?=(\d{3})+(?:\.\d|\b)))/g, "\$1" + separator);
-        r = (rr[1] ? b + '.' + rr[1] : b);
-
-        return r;
-    }
 
 })
 
